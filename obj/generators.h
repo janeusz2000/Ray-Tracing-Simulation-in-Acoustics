cb4e60e1d93bccc3e8d39660af30ff9f2c192bcf
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-10-05 15:20:01 +0200

    adding declaration of the pointSource Class

--- obj/generators.h
+++ obj/generators.h
@@ -6,9 +6,30 @@
 #include "constants.h"
 #include "core/exceptions.h"
 
+#include <initializer_list>
+#include <iostream>
+
 namespace generators
 {
+    class PointSource // This object is similar to Camera in graphics ray-tracer, that why name convension is simillar to camera
+    {
+    public:
+        PointSource();
+        PointSource(const core::Vec3 &dir);
+        PointSource(const core::Vec3 &dir, const core::Vec3 &pos);
+        PointSource(const std::initializer_list<double> &dir, const std::initializer_list<double> &pos);
+        PointSource(const PointSource &) = default;
+        ~PointSource() = default;
 
-};
+        // OPERATORS
+        PointSource &operator=(const PointSource &) = default;
+        bool operator==(const PointSource &other) const;
+        friend std::ostream &operator<<(std::ostream &os, const PointSource &pointSource);
+
+    private:
+        double _aspectRatio, _focalLength;
+        core::Vec3 _origin, _upperLeftCorner;
+    };
+}; // namespace generators
 
 #endif
