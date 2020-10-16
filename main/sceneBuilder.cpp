4f1ddbbcf35a3810f6ee814704b6307a05fd4604
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-10-16 17:36:42 +0200

    adding more test for testSample

--- main/sceneBuilder.cpp
+++ main/sceneBuilder.cpp
@@ -13,8 +13,36 @@ using objects::TriangleObj;
 const double kSkipFreq = 1000;
 generators::FakeRandomGen fakeRand;
 
-TEST(SceneBuilderTest, PointSourceRaysHitsBuildedTestSample) // Monte Carlo test: https://en.wikipedia.org/wiki/Monte_Carlo_method
+TEST(SceneBuildertest, IsTestSampleRightShape)
 {
+    Vec3 k1(-0.5, -0.5, 0);
+    Vec3 k2(0.5, -0.5, 0);
+    Vec3 k3(-0.5, 0.5, 0);
+    Vec3 k4(0.5, 0.5, 0);
+    Vec3 k5(123, 123, 1);
+
+    SceneBuilder builder;
+
+    const std::vector<TriangleObj> referenceSample = {TriangleObj(k1, k2, k3), TriangleObj(k2, k3, k4)};
+
+    // because how operator== in triangleObj is implemented, I can do that:
+
+    std::vector<std::unique_ptr<Object>> testSample = builder.BuildTestSample();
+    ASSERT_TRUE(std::find(referenceSample.cbegin(), referenceSample.cend(), *testSample[0]) != referenceSample.cend() &&
+                std::find(referenceSample.cbegin(), referenceSample.cend(), *testSample[1]) != referenceSample.cend())
+        << "Triangle1: " << *testSample[0] << ", Triangle2: " << *testSample[1];
+
+    const std::vector<TriangleObj> invalidTestSample = {TriangleObj(k1, k2, k3), TriangleObj(k2, k3, k5)};
+    ASSERT_FALSE(std::find(invalidTestSample.cbegin(), invalidTestSample.cend(), *testSample[0]) != invalidTestSample.cend() &&
+                 std::find(invalidTestSample.cbegin(), invalidTestSample.cend(), *testSample[1]) != referenceSample.cend())
+        << "Triangle1: " << *testSample[0] << ", Triangle2: " << *testSample[1];
+}
+
+TEST(SceneBuilderTest, PointSourceRaysHitsBuildedTestSample)
+{
+    // ! IM NOT SURE THIS IS USEFULL BUT HEEEEEY ;-)
+    // Monte Carlo test: https://en.wikipedia.org/wiki/Monte_Carlo_method
+
     const size_t rayNumberPerRow = 10000;
     const double sampleSide = 1;
     PointSource source(kSkipFreq, rayNumberPerRow, sampleSide, fakeRand);
