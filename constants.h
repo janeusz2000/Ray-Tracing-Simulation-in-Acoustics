f9124a9b9d19e0691c662ff38dd7d0b36ad7c56f
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-10-19 21:23:16 +0200

    adding even more TODO's ;P

--- constants.h
+++ constants.h
@@ -8,8 +8,8 @@ namespace constants
     const double kAccuracy = 0.00000001;
     const double kSoundSpeed = 343.216; // This is measured in 20'C at 1000 hPa
     const double kPi = std::acos(-1);
-    const double kSimulationRadius = 4; // [m]
-    const int kPopulation = 37;
+    const double kSimulationRadius = 4; // [m] // TODO: deletethis dependency
+    const int kPopulation = 37; // TODO: delete this dependency
     const double kHitAccuracy = 0.0001;
 
 
