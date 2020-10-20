acb785d9eebda4b45a2e93bd34be0d93869c8874
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-10-20 20:55:33 +0200

    deleteing one TODO from exception.h

--- core/exceptions.h
+++ core/exceptions.h
@@ -7,8 +7,6 @@
 
 namespace exception
 {   
-    // TODO: Change all doubles to float
-    // TODO: change underscore to the end
 
     // TODO: Make sure that exception::objectsUnableToImport this error provide enough information about the error occurred
     class objectUnableToImport : public std::exception
