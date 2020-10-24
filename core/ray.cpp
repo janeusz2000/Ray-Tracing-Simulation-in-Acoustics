909d77ecc6823ff759a4d66447ff0c8da15e319d
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-10-24 12:00:19 +0200

    deleting pragma region from ray.cpp

--- core/ray.cpp
+++ core/ray.cpp
@@ -3,7 +3,6 @@
 #include <sstream>
 
 namespace core {
-#pragma region CONSTRUCTORS
 
 Ray::Ray(const Vec3 &origin, const Vec3 &direction, float energy) {
   if (direction == Vec3(0, 0, 0)) {
@@ -14,9 +13,6 @@ Ray::Ray(const Vec3 &origin, const Vec3 &direction, float energy) {
   direction_ = direction.normalize();
   energy_ = energy;
 }
-
-#pragma endregion
-#pragma region METHODS
 
 Vec3 Ray::at(float time) const { return origin_ + time * direction_; }
 
@@ -32,9 +28,6 @@ float Ray::phaseAt(float freq, float time) const {
   return time / waveLength * 2 * constants::kPi;
 }
 
-#pragma endregion
-#pragma region OPERATORS
-
 std::ostream &operator<<(std::ostream &os, const Ray &srcRay) {
   return os << "RAY origin: " << srcRay.getOrigin()
             << ", direction: " << srcRay.getDirection()
@@ -45,9 +38,6 @@ bool operator==(const Ray &left, const Ray &right) {
   return (left.getOrigin() == right.getOrigin() &&
           left.getDirection() == right.getDirection());
 }
-
-#pragma endregion
-#pragma region SETTERS_GETTERS
 
 void Ray::setOrigin(const Vec3 &origin) { origin_ = origin; }
 
@@ -62,9 +52,6 @@ Vec3 Ray::getDirection() const { return direction_; }
 void Ray::setEnergy(float num) { energy_ = num; }
 
 float Ray::getEnergy() const { return energy_; }
-
-#pragma endregion
-#pragma region RAYHITDATA
 
 bool RayHitData::operator==(const RayHitData &other) const {
   return (std::abs(other.time() - time()) <= constants::kAccuracy &&
@@ -83,5 +70,4 @@ std::ostream &operator<<(std::ostream &os, const RayHitData &rayData) {
             << " [radians]";
 }
 
-#pragma endregion
 } // namespace core
