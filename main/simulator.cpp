d5fc16eff5a4bea3a575be868397062b3542b36c
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-01 01:40:21 +0100

    adding more declaration for the future. Im now plannign how Simulator will work, this can be changed

--- main/simulator.cpp
+++ main/simulator.cpp
@@ -1,1 +1,9 @@
 #include "main/simulator.h"
+
+void Simulator::run() {
+  // TODO: runs simulation
+}
+
+void Simulator::calculatePressure() {
+  // TODO: calculates all pressure from energy collectors
+}
