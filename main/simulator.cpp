38d3a2d7a9abdee2cd0c70b3895501d74620367e
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-11 10:46:24 +0100

    fixing collector PShere radius implementation to be more clear

--- main/simulator.cpp
+++ main/simulator.cpp
@@ -23,36 +23,35 @@ buildCollectors(const ModelInterface &model, int numCollectors) {
     throw std::invalid_argument(ss.str());
   }
 
-  // Minimum distance from Vec3(0, 0, 0) that each collector must meet.
-  // Value of the minDistance can't be less then 4 [m]
-  float minDistance =
-      4 * std::max<float>(
-              {model.height(), model.sideSize(),
-               /*makes sure that minDistance is at least equal to 4 */ 1.0f});
-
   // For even number of ray collectors, in order to equally cover two simulation
-  // half-circle (half-circle that cross each collectors origin), we need to
-  // calculate angle between Vec3(0, 0, 0) and 2 points where energy collector
-  // radius will cross simulation half-circle. Calculation will varies depending
-  // on if number of collectors is even or not.
+  // half-circle (half-circle that cross each collectors
+  // origin), we need to  calculate angle between Vec3(0, 0, 0) and 2 points
+  // where energy collector radius will cross simulation half-circle.
+  // Calculation will varies depending on if number of collectors is even or
+  // not.
   const int numCollectorIsOdd = numCollectors % 2;
   const float angle = 2 * constants::kPi /
                       static_cast<float>(numCollectors + numCollectorIsOdd - 2);
+
+  // radius of the two half-circles  is equal to 4 times the biggest dimension
+  // of the model and must be at least equal to 4.
+  float collectorSphereRadius =
+      std::max(4.0f, 4 * std::max(model.height(), model.sideSize()));
 
   // After gathering previously mentioned angle, we can calculate radius of each
   // energy collector, from Law of Cosines:
   // https://en.wikipedia.org/wiki/Law_of_cosines
   // and after couple fo transformations formula will look like this:
-  // r 0= R * sqrt(2 - 2 * cos(angle))
+  // r = R * sqrt(2 - 2 * cos(angle))
   float energyCollectorRadius =
-      minDistance * static_cast<float>(std::sqrt(2 - 2 * std::cos(angle)));
+      collectorSphereRadius * std::sqrt(2.0f - 2 * std::cos(angle));
 
   std::vector<std::unique_ptr<objects::EnergyCollector>> energyCollectors;
   // When Num Collectors is not even, we need to put one collector at (0, 0,
   // minRadius)`
   if (numCollectorIsOdd) {
     energyCollectors.push_back(std::make_unique<objects::EnergyCollector>(
-        core::Vec3(0, 0, minDistance), energyCollectorRadius));
+        core::Vec3(0, 0, collectorSphereRadius), energyCollectorRadius));
   }
   // and decrease number remaining collectors to create remaining ones
   const int numToGo = numCollectors - (numCollectorIsOdd);
@@ -61,8 +60,8 @@ buildCollectors(const ModelInterface &model, int numCollectors) {
   std::vector<core::Vec3> origins;
   for (int times = 0; times * 4 < numToGo; ++times) {
 
-    float groundCoordinate = minDistance * std::cos(times * angle);
-    float zCoordinate = minDistance * std::sin(times * angle);
+    float groundCoordinate = collectorSphereRadius * std::cos(times * angle);
+    float zCoordinate = collectorSphereRadius * std::sin(times * angle);
 
     origins = {core::Vec3(groundCoordinate, 0, zCoordinate),
                core::Vec3(-groundCoordinate, 0, zCoordinate),
