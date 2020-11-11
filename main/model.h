26b4403a248637b03033a91e5349dfae4a320611
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-11 15:52:17 +0100

    adding comments for model object in simulation

--- main/model.h
+++ main/model.h
@@ -6,29 +6,30 @@
 #include <memory>
 #include <vector>
 
+// Holds all Triangle Objects that represents model in simulation and adds
+// utilities for it.
 class ModelInterface {
 public:
   virtual ~ModelInterface(){};
-  // Model that simulation will be performend on
   virtual std::vector<objects::TriangleObj *> triangles() const = 0;
   virtual float height() const = 0;
   virtual float sideSize() const = 0;
+  // returns true if there is no object assigned to a model.
   virtual bool empty() const = 0;
 };
 
 class Model : public ModelInterface {
 public:
-  // Model that simulation will be performend on
   std::vector<objects::TriangleObj *> triangles() const;
-
-  static std::unique_ptr<Model> NewLoadFromObjectFile(std::string_view path);
-  static std::unique_ptr<Model> NewReferenceModel(float sideSize);
 
   float height() const { return height_; }
   float sideSize() const { return sideSize_; }
   bool empty() const override {
     return std::max<float>(height(), sideSize()) <= constants::kAccuracy;
   }
+
+  static std::unique_ptr<Model> NewLoadFromObjectFile(std::string_view path);
+  static std::unique_ptr<Model> NewReferenceModel(float sideSize);
 
 private:
   std::vector<std::unique_ptr<objects::TriangleObj>> triangles_;
