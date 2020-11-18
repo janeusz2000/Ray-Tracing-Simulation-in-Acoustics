43e508df1cce2e237bdf84d180bf2b969884d0b7
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-18 17:06:28 +0100

    refractoring tests

--- tests/sphere_test.cpp
+++ tests/sphere_test.cpp
@@ -35,16 +35,21 @@ TEST_F(SphereCollisionTest, RayHitFromOutsideSphere) {
   Sphere sphere(kVecZero, 1);
   RayHitData hitData;
 
+  Vec3 inFrontOfSphereYAxis = -4 * kVecY;
+  ASSERT_FALSE(sphere.isVecInside(inFrontOfSphereYAxis));
   Ray alongYAxis(Vec3(0, -4, 0), kVecY);
   ASSERT_TRUE(sphere.hitObject(alongYAxis, kSkipFreq, &hitData));
   ASSERT_FLOAT_EQ(alongYAxis.origin().magnitude() - sphere.getRadius(),
                   hitData.time);
 
-  Ray alongYAxisOppposite(Vec3(0, 4, 0), kVecY);
+  Vec3 behindSphereYAxis = 4 * kVecY;
+  ASSERT_FALSE(sphere.isVecInside(behindSphereYAxis));
+  Ray alongYAxisOppposite(behindSphereYAxis, kVecY);
   ASSERT_FALSE(sphere.hitObject(alongYAxisOppposite, kSkipFreq, &hitData))
       << "hit at: " << hitData.collisionPoint();
 
   Vec3 arbitraryVec3(12.123, 5.623, 13.235);
+  ASSERT_FALSE(sphere.isVecInside(arbitraryVec3));
   Ray arbitraryOriginRay(arbitraryVec3, sphere.getOrigin() - arbitraryVec3);
   ASSERT_TRUE(sphere.hitObject(arbitraryOriginRay, kSkipFreq, &hitData));
   ASSERT_NEAR((arbitraryVec3 - sphere.getOrigin()).magnitude() -
@@ -54,14 +59,24 @@ TEST_F(SphereCollisionTest, RayHitFromOutsideSphere) {
   // This comes from proportions of the pitagorus triangle where sides satisfy
   // the equation : 3^2 + 4^2 = 5^5
   Vec3 originFromAngle(0, -3, -4);
+  ASSERT_FALSE(sphere.isVecInside(originFromAngle));
   Ray rayToSphereOriginFromAngle(originFromAngle,
                                  sphere.getOrigin() - originFromAngle);
   ASSERT_TRUE(
       sphere.hitObject(rayToSphereOriginFromAngle, kSkipFreq, &hitData));
   ASSERT_FLOAT_EQ(5 - sphere.getRadius(), hitData.time);
 
-  Ray alongXOneHit(Vec3(-5, 0, 1), kVecX);
+  Vec3 alongXOneHitOrigin = Vec3(-5, 0, 1);
+  ASSERT_FALSE(sphere.isVecInside(alongXOneHitOrigin));
+  Ray alongXOneHit(alongXOneHitOrigin, kVecX);
   ASSERT_TRUE(sphere.hitObject(alongXOneHit, kSkipFreq, &hitData));
+  ASSERT_EQ(5, hitData.time);
+  ASSERT_EQ(Vec3(0, 0, 1), hitData.collisionPoint());
+
+  Vec3 alongXOneHitOriginMiss = Vec3(-5, 0, 1 + constants::kAccuracy);
+  ASSERT_FALSE(sphere.isVecInside(alongXOneHitOriginMiss));
+  Ray alongXOneHitMiss(alongXOneHitOriginMiss, kVecX);
+  ASSERT_FALSE(sphere.hitObject(alongXOneHitMiss, kSkipFreq, &hitData));
   ASSERT_EQ(5, hitData.time);
   ASSERT_EQ(Vec3(0, 0, 1), hitData.collisionPoint());
 }
@@ -96,24 +111,11 @@ TEST_F(SphereCollisionTest, RayAtEdgeOfSphereDontHit) {
   Sphere sphere(kVecZero, 1);
   RayHitData hitData;
 
-  Ray alongXAxis(Vec3(1, 0, 0), kVecX);
+  Ray alongXAxis(kVecX, kVecX);
   ASSERT_FALSE(sphere.hitObject(alongXAxis, kSkipFreq, &hitData))
       << "hit at: " << hitData.collisionPoint();
 
   Ray alongXAxisHit(Vec3(1 - constants::kAccuracy, 0, 0), kVecX);
   ASSERT_TRUE(sphere.hitObject(alongXAxisHit, kSkipFreq, &hitData));
   ASSERT_NEAR(constants::kAccuracy, hitData.time, constants::kAccuracy);
-}
-
-TEST_F(SphereCollisionTest, RayMissSphere) {
-  Sphere sphere(Vec3(15, 0, 0), 1);
-  RayHitData hitData;
-
-  Ray nextToSphere(Vec3(14 - constants::kAccuracy, 0, -5), kVecUp);
-  ASSERT_FALSE(sphere.hitObject(nextToSphere, kSkipFreq, &hitData))
-      << "hit at: " << hitData.collisionPoint();
-
-  Ray nextToSphereHit(Vec3(14, 0, -5), kVecUp);
-  ASSERT_TRUE(sphere.hitObject(nextToSphereHit, kSkipFreq, &hitData));
-  ASSERT_FLOAT_EQ(5, hitData.time);
 }
