78299c0eab2d3e74457e8780a4c7af014db821f0
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-18 15:41:18 +0100

    refractored all tests

--- tests/sphere_test.cpp
+++ tests/sphere_test.cpp
@@ -103,7 +103,7 @@ TEST_F(SphereCollisionTest, RayAtEdgeOfSphereDontHit) {
   ASSERT_FALSE(sphere.hitObject(alongXAxis, kSkipFreq, &hitData))
       << "hit at: " << hitData.collisionPoint();
 
-  Ray alongXAxisHit(Vec3(1 - constants::kAccuracy, 0, 0), Vec3(1, 0, 0));
+  Ray alongXAxisHit(Vec3(1 - constants::kAccuracy, 0, 0), kVecX);
   ASSERT_TRUE(sphere.hitObject(alongXAxisHit, kSkipFreq, &hitData));
   ASSERT_NEAR(constants::kAccuracy, hitData.time, constants::kAccuracy);
 }
