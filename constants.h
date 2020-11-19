15222e48b18abc87a08f805f7ae8588a2b425e70
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-19 11:11:51 +0100

    nw should be fine

--- constants.h
+++ constants.h
@@ -4,7 +4,7 @@
 #include <cmath>
 
 namespace constants {
-// kAccuracy express accuracy in meters in calculation of hit position, 
+// kAccuracy express accuracy in meters in hit position calculation, 
 // or accuracy of time in seconds in hit time calculation
 const float kAccuracy = 0.00005; 
 // This is measured in 20'C at 1000 hPa
