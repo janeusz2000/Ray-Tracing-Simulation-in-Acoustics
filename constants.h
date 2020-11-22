24fb591dae1866ab354d39849395d890f390cb02
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-22 13:47:38 +0100

    fixing grammar and language in comments

--- constants.h
+++ constants.h
@@ -4,10 +4,10 @@
 #include <cmath>
 
 namespace constants {
-// kAccuracy express accuracy in meters in hit position calculation,
+// Expresses accuracy in meters in hit position calculation,
 // or accuracy of time in seconds in hit time calculation
 const float kAccuracy = 0.00005;
-// This is measured in 20'C at 1000 hPa
+// Measured in 20'C at 1000 hPa
 const float kSoundSpeed = 343.216;
 const float kPi = std::acos(-1);
 
