1dcac3c0f0662688305c9e12375f1d84e9e13957
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-11-22 13:50:03 +0100

    fixing more grammar

--- constants.h
+++ constants.h
@@ -4,8 +4,8 @@
 #include <cmath>
 
 namespace constants {
-// Expresses accuracy in meters in hit position calculation,
-// or accuracy of time in seconds in hit time calculation
+// Expresses accuracy of measured distance in meters in hit position
+// calculation, or accuracy of time in seconds in hit time calculation
 const float kAccuracy = 0.00005;
 // Measured in 20'C at 1000 hPa
 const float kSoundSpeed = 343.216;
