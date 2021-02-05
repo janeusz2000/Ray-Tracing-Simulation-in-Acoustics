ae78e2274ed91cbaecf242d0b4d7087a322ae6b4
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-02-05 21:09:02 +0100

    addding web configuration for gui

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+module.exports = {
+  mode : 'development',
+  devtool : false,
+  entry : './gui/index.js',
+  output : {filename : 'main.js', path : path.resolve(__dirname, 'dist')}
+}
