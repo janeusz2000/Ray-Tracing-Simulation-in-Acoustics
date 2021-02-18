5b73716dc566601a03a1d61b31d32ccc972d7515
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-02-18 18:52:52 +0100

    created data class that hold all properties of the simulation

--- main/sceneManager.cpp
+++ main/sceneManager.cpp
@@ -1,15 +1,11 @@
 #include "main/sceneManager.h"
 
-SceneManager::SceneManager(
-    Model *model, const std::vector<float> &frequencies,
-    std::string_view dataPath,
-    collectionRules::CollectEnergyInterface *energyCollectionRules,
-    float sourcePower, int numOfCollectors, int numOfRaysSquared)
-    : model_(model), frequencies_(frequencies),
-      energyCollectionRules_(energyCollectionRules),
-      numOfCollectors_(numOfCollectors), raytracer_(model),
-      tracker_(dataPath.data()),
-      pointSpeaker_(numOfRaysSquared, sourcePower, model) {
+SceneManager::SceneManager(Model *model,
+                           const SimulationProperties &simulationProperties)
+    : model_(model), simulationProperties_(simulationProperties),
+      raytracer_(model), tracker_(simulationProperties.dataPath()),
+      pointSpeaker_(simulationProperties.numOfRaySquared(),
+                    simulationProperties.sourcePower(), model) {
 
   offseter_ = std::make_unique<generators::FakeOffseter>();
   referenceModel_ = Model::NewReferenceModel(model->sideSize());
@@ -17,7 +13,7 @@ SceneManager::SceneManager(
 
 void SceneManager::run() {
   Simulator simulator(&raytracer_, model_, &pointSpeaker_, offseter_.get(),
-                      &tracker_, energyCollectionRules_);
+                      &tracker_, simulationProperties_.energyCollectionRules());
 
   // TODO: multicore energy calculation -> when core is available, use it
   // TODO: export data to Json
--- main/sceneManager.h
+++ main/sceneManager.h
@@ -12,22 +12,48 @@
 #include <string_view>
 #include <vector>
 
+struct SimulationProperties {
+  explicit SimulationProperties(
+      std::string_view dataPath, const std::vector<float> &frequencies /*[Hz]*/,
+      collectionRules::CollectEnergyInterface *energyCollectionRules,
+      float sourcePower = 500 /*[W]*/, int numOfCollectors = 37,
+      int numOfRaySquared = 1000)
+      : dataPath_(dataPath.data()), frequencies_(frequencies),
+        energyCollectionRules_(energyCollectionRules),
+        sourcePower_(sourcePower), numOfCollectors_(numOfCollectors),
+        numOfRaySquared_(numOfRaySquared) {}
+
+  const std::vector<float> frequencies() const { return frequencies_; }
+  collectionRules::CollectEnergyInterface *energyCollectionRules() {
+    return energyCollectionRules_;
+  }
+  const float sourcePower() const { return sourcePower_; }
+  const int numOfCollectors() const { return numOfCollectors_; }
+  const int numOfRaySquared() const { return numOfRaySquared_; }
+  const char *dataPath() const { return dataPath_.data(); }
+  collectionRules::CollectEnergyInterface *collectionRules() {
+    return energyCollectionRules_;
+  }
+
+private:
+  std::string_view dataPath_;
+  std::vector<float> frequencies_;
+  collectionRules::CollectEnergyInterface *energyCollectionRules_;
+  float sourcePower_;
+  int numOfCollectors_;
+  int numOfRaySquared_;
+};
+
 // This class is creating all necessary objects for simulation.
 class SceneManager {
 public:
-  SceneManager(Model *model, const std::vector<float> &frequencies /*[Hz]*/,
-               std::string_view dataPath,
-               collectionRules::CollectEnergyInterface *energyCollectionRules,
-               float sourcePower = 500 /*[W]*/, int numOfCollectors = 37,
-               int numOfRayRaysSquared = 1000);
+  SceneManager(Model *model, const SimulationProperties &simulationProperties);
 
   void run();
 
 private:
   Model *model_;
-  std::vector<float> frequencies_;
-  collectionRules::CollectEnergyInterface *energyCollectionRules_;
-  int numOfCollectors_;
+  SimulationProperties simulationProperties_;
 
   RayTracer raytracer_;
   trackers::PositionTracker tracker_;
