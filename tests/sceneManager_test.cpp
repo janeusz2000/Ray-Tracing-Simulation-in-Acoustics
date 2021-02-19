36905dbe87df2331a39c3c9cf2ad93d27b08f0b7
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-02-19 20:02:31 +0100

    fixing test, refractoring, ect

--- tests/sceneManager_test.cpp
+++ tests/sceneManager_test.cpp
@@ -59,5 +59,5 @@ TEST_F(SceneManagerSimpleTest, repetitiveCollectionOfEnergyTest) {
                                    &positionTracker, &collectorsTracker);
   EnergiesPerFrequency result = singleRaySimulation.run();
   ASSERT_THAT(result[0], ::testing::Not(::testing::IsEmpty()));
-  ASSERT_THAT(result[0], result[1]);
+  ASSERT_EQ(result[0], result[1]);
 }
