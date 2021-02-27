17b7e3de81d740139cb5853d87b9dd25ff55ac55
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-02-27 14:07:52 +0100

    fixing webpack multile targets

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
 
-module.exports = {
+const simulation = {
   mode : 'development',
   devtool : false,
   entry : './gui/index.js',
   output : {filename : 'main.js', path : path.resolve(__dirname, 'dist')},
 };
 
-// module.exports = {
-//   mode : 'development',
-//   devtool : false,
-//   entry : './gui/results.js',
-//   output : {filename : 'results.js', path : path.resolve(__dirname, 'dist')},
-// };
+const results = {
+  mode : 'development',
+  devtool : false,
+  entry : './gui/results.js',
+  output : {filename : 'results.js', path : path.resolve(__dirname, 'dist')},
+};
+
+module.exports = [ simulation, results ];
