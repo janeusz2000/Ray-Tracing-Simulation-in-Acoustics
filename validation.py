f6c522fd2cd6ac83cb130f02d1312138ac43dbb4
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-07-10 00:41:26 +0200

    moving into python script for validation run

---
+++ validation.py
@@ -0,0 +1,122 @@
+import pprint
+import json
+import os
+import logging
+from dataclasses import dataclass
+
+
+@dataclass
+class DiffusorProperty:
+    name: str
+    path: str
+    type: str
+
+
+@dataclass
+class SimulationProperties:
+    sourcePower: float
+    numOfCollectors: int
+    numOfRaysSquared: int
+    maxTracking: int
+
+
+def setUpLogger() -> logging.Logger:
+    logger = logging.getLogger('Validaton Logger')
+    logger.setLevel(logging.DEBUG)
+
+    channel = logging.StreamHandler()
+    channel.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    channel.setFormatter(formatter)
+    logger.addHandler(channel)
+    return logger
+
+
+def executeCommand(command: str) -> None:
+    os.system(command)
+
+
+def buildBinary():
+    os.system("bazel build --config=_gcc validation")
+
+
+def executeLineValidation(diffusorProperty: DiffusorProperty,
+                          simulationProperties: SimulationProperties,
+                          raportPath: str) -> None:
+    command = f"bazel-bin/validation {diffusorProperty.type} {raportPath} {diffusorProperty.path} {simulationProperties.sourcePower} {simulationProperties.numOfCollectors} {simulationProperties.numOfRaysSquared} {simulationProperties.maxTracking}"
+    executeCommand(command)
+
+
+def executeSurfaceValidation(diffusorProperty: DiffusorProperty,
+                             simulationProperties: SimulationProperties,
+                             raportPath: str) -> None:
+    command = f"bazel-bin/validation geodome {raportPath} {diffusorProperty.path} {simulationProperties.sourcePower} {simulationProperties.numOfCollectors} {simulationProperties.numOfRaysSquared} {simulationProperties.maxTracking}"
+    executeCommand(command)
+
+
+def getDiffusorPropertyList() -> list:
+    diffusorPropertiesList = list()
+    for dirpath, dnames, fNames in os.walk("./validationDiffusors"):
+        for f in fNames:
+            if f.endswith(".obj"):
+                diffusorType = "xAxis" if "1D" in f else "xyAxis"
+                diffusor = DiffusorProperty(
+                    f[:-4], os.path.join(dirpath, f), diffusorType)
+                diffusorPropertiesList.append(diffusor)
+    return diffusorPropertiesList
+
+
+def createLineRaportFile(name: str) -> str:
+    path = f"./ValidationResults/rayTraced/line/{name}.json"
+    executeCommand("rm " + path)
+    executeCommand("touch " + path)
+    return path
+
+
+def createSurfaceRaportFile(name: str) -> str:
+    path = f"./ValidationResults/rayTraced/surface/{name}.json"
+    executeCommand("rm " + path)
+    executeCommand("touch " + path)
+    return path
+
+
+def getLineReferencePath(name: str) -> str:
+    return f"./validationResults/Reference/line/{name}.json"
+
+
+def getSurfaceReferencePath(name: str) -> str:
+    return f"./validationResults/Reference/surface/{name}.json"
+
+
+if __name__ == "__main__":
+    logger = setUpLogger()
+
+    logger.info("Starting validation")
+    simulationProperties = SimulationProperties(
+        sourcePower=1000,
+        numOfCollectors=33,
+        numOfRaysSquared=5,
+        maxTracking=15)
+
+    logger.info(f"Configuration:")
+    logger.info(f"\tSource Power {simulationProperties.sourcePower} [W]")
+    logger.info(f"\tNum Of collectors: {simulationProperties.numOfCollectors}")
+    logger.info(f"\tNum of rays: {simulationProperties.numOfRaysSquared**2}")
+    logger.info(f"\tMax tracking: {simulationProperties.maxTracking}")
+
+    DiffusorPropertyList = getDiffusorPropertyList()
+    for diffusor in DiffusorPropertyList:
+        lineRaportPath = createLineRaportFile(diffusor.name)
+        executeLineValidation(diffusorProperty=diffusor,
+                              simulationProperties=simulationProperties,
+                              raportPath=lineRaportPath)
+        lineReferencePath = getLineReferencePath(diffusor.name)
+
+        surfaceRaportPath = createSurfaceRaportFile(diffusor.name)
+        executeSurfaceValidation(diffusorProperty=diffusor,
+                                 simulationProperties=simulationProperties,
+                                 raportPath=surfaceRaportPath)
+        surfaceReferencePath = getSurfaceReferencePath(diffusor.name)
--- validation.sh
+++ validation.sh
@@ -6,7 +6,8 @@ source ./venv/bin/activate
 
 SOURCE_POWER=1000
 NUM_OF_COLLECTORS=33
-NUM_OF_RAYS_SQUARED=10
+NUM_OF_RAYS_SQUARED=5
+MAX_TRACKING=15
 
 echo STARTING VALIDATION FOR:
 echo - SOURCE POWER: $SOURCE_POWER
@@ -22,25 +23,26 @@ for DIFFUSOR_PATH in $DIFFUSORS_ARRAY_PATH; do
 
    CURRENT_FILE=$(echo ${DIFFUSOR_PATH%.*} | cut -c21-)
 
-   RAPORT_LINE_FILE="./ValidationResults/rayTraced/line/$CURRENT_FILE.json"
+   RAPORT_LINE_FILE=""
    REFERENCE_FILE_LINE="./validationResults/Reference/line/$CURRENT_FILE.json"
    rm $RAPORT_LINE_FILE
    touch $RAPORT_LINE_FILE
 
-   if [[$CURRENT_FILE == *"2D"*]]; then
-      bazel-bin/validation "xyAxis" $RAPORT_LINE_FILE "./$DIFFUSOR_PATH" $SOURCE_POWER $NUM_OF_COLLECTORS $NUM_OF_RAYS_SQUARED
+   if [[ $CURRENT_FILE == "*2D*" ]]; then
+      bazel-bin/validation "xyAxis" $RAPORT_LINE_FILE ./$DIFFUSOR_PATH $SOURCE_POWER $NUM_OF_COLLECTORS $NUM_OF_RAYS_SQUARED $MAX_TRACKING
       python3 ./validationTools/compareResutlsToReference.py $REFERENCE_FILE_LINE $RAPORT_FILE_LINE "Line"
    else
-      bazel-bin/validation "xAxis" $RAPORT_LINE_FILE "./$DIFFUSOR_PATH" $SOURCE_POWER $NUM_OF_COLLECTORS $NUM_OF_RAYS_SQUARED
+      bazel-bin/validation "xAxis" $RAPORT_LINE_FILE "./$DIFFUSOR_PATH" $SOURCE_POWER $NUM_OF_COLLECTORS $NUM_OF_RAYS_SQUARED $MAX_TRACKING
       python3 ./validationTools/compareResutlsToReference.py $REFERENCE_FILE_LINE $RAPORT_FILE_LINE "Line"
    fi
 
    RAPORT_SURFACE_FILE="./ValidationResults/RayTraced/surface/$CURRENT_FILE.json"
    REFERENCE_FILE_SURFACE="./validationResults/Reference/surface/$CURRENT_FILE.json"
+
    rm $RAPORT_SURFACE_FILE
    touch $RAPORT_SURFACE_FILE
 
-   bazel-bin/validation "geoDome" $RAPORT_LINE_FILE "./$DIFFUSOR_PATH" $SOURCE_POWER $NUM_OF_COLLECTORS $NUM_OF_RAYS_SQUARED
+   bazel-bin/validation "geoDome" $RAPORT_LINE_FILE "./$DIFFUSOR_PATH" $SOURCE_POWER $NUM_OF_COLLECTORS $NUM_OF_RAYS_SQUARED $MAX_TRACKING
    python3 ./validationTools/compareResutlsToReference.py $REFERENCE_FILE_SURFACE $RAPORT_FILE_SURFACE "Surface"
 done
 
